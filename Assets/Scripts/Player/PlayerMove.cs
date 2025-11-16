d1915c05db4f347b34d9f83414d0a67ea450129e
DylanDixon-Guilty <dylan.dixon@g.austincc.edu>
2025-11-16 15:26:48 -0600

    Created a player move script.

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,32 +2,61 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Transform player;
     [SerializeField] private Vector3 movement;
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private float cameraVerticalRotation = 0f;
     [SerializeField] private const string moveHorizontal = "Horizontal";
     [SerializeField] private const string moveVertical = "Vertical";
-    [SerializeField] private const string cameraRotateAxis = "Mouse X";
+    [SerializeField] private const string mouseAxisX = "Mouse X";
+    [SerializeField] private const string mouseAxisY = "Mouse Y";
+    
+    // This is for when the player is interacting with a puzzle.
+    // If it is locked, the player can rotate and move around the world.
+    // If it is not locked, do not let the player rotate or move the player.
+    // Since they interacting with a puzzle and need the mouse.
+    [SerializeField] private bool isCursorLocked = true;
 
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
         Move();
-        Rotate();
+        Rotation();
     }
 
     private void Move()
     {
-        float horizontal = Input.GetAxisRaw(moveHorizontal);
-        float vertical = Input.GetAxisRaw(moveVertical);
-        movement.Set(horizontal, 0f, vertical);
-        movement = movement.normalized * moveSpeed * Time.deltaTime;
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        if(isCursorLocked)
+        {
+            float horizontal = Input.GetAxisRaw(moveHorizontal);
+            float vertical = Input.GetAxisRaw(moveVertical);
+            movement.Set(horizontal, 0f, vertical);
+            movement = movement.normalized * moveSpeed * Time.deltaTime;
+            transform.Translate(movement * moveSpeed * Time.deltaTime);
+        }
     }
 
-    private void Rotate()
+    private void Rotation()
     {
-        float rotateInput = Input.GetAxis(cameraRotateAxis);
-        transform.Rotate(Vector3.up, rotateInput * rotationSpeed * Time.deltaTime);
+        if(isCursorLocked)
+        {
+            float inputX = Input.GetAxis(mouseAxisX) * mouseSensitivity;
+            float inputY = Input.GetAxis(mouseAxisY) * mouseSensitivity;
+
+            // To rotate the camera up and down
+            cameraVerticalRotation -= inputY;
+            cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+            transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+            // To rotate the camera left and right
+            player.Rotate(Vector3.up * inputX);
+        }
     }
 }
